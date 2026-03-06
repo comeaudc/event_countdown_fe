c8869f49654ef63787c6bd3d91606f098ae4aa50
DylanC <comeaudc@comcast.net>
2026-03-06 10:29:38 -0500

    Setup invite JSX functionality

--- src/pages/Invite/Invite.jsx
+++ src/pages/Invite/Invite.jsx
@@ -1,3 +1,59 @@
-export default function Invite(){
-    return <h1>Invite</h1>
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Invite() {
+  const nav = useNavigate();
+  const { token } = useParams();
+  const [guest, setGuest] = useState(null);
+
+  async function getUserData() {
+    try {
+      let res = await axios.get(`http://localhost:3000/api/invite/${token}`);
+
+      setGuest(res.data);
+      localStorage.setItem("inviteToken", token);
+
+      if (res.data.rsvp) nav("/");
+    } catch (error) {
+      console.error(error);
+      alert(`${error.message}`);
+    }
+  }
+
+  async function handleRSVP(e) {
+    try {
+      if (e.target.value == "Yes") {
+        await axios.post(`http://localhost:3000/api/rsvp`, { attending: true }, {headers: {authorization: token}});
+        nav("/");
+      } else {
+        window.location.href =
+          "https://www.youtube.com/watch?v=Aq5WXmQQooo&pp=ygUJcmljayByb2xs";
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, [token]);
+
+  return guest ? (
+    <>
+      <h1>Hi {guest.name}!!!</h1>
+      <h1>You are invited to Dylan & Christinas Wedding! 💍</h1>
+      <fieldset>
+        <legend>
+          <strong>RSVP</strong>
+        </legend>
+        <form>
+          <input onClick={handleRSVP} type="button" value="Yes" />
+          <input onClick={handleRSVP} type="button" value="No" />
+        </form>
+      </fieldset>
+    </>
+  ) : (
+    <h1>Loading</h1>
+  );
 }
