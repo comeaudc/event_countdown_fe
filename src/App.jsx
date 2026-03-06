ccccfebf9d583681ebe8d380510ee2e78b20c26a
DylanC <comeaudc@comcast.net>
2026-03-06 09:19:54 -0500

    Set up base CSS and routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Invite from "./pages/Invite/Invite";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Gallery from "./pages/Gallery/Gallery";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="" element={<Invite />} />
-        <Route path="" element={<Dashboard />} />
-        <Route path="" element={<Gallery />} />
+        <Route path="/invite/:token" element={<Invite />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </>
   );
